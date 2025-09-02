5b69f42036d06d08c2a3bc7b66579edd9f1f70af
Hakjun Lee <leehakjun111@naver.com>
2025-09-02 20:05:14 +0900

    setting

--- src/PracticePad.java
+++ src/PracticePad.java
@@ -1,5 +1,16 @@
 public class PracticePad {
     public static void main(String[] args) throws Exception {
-        
+        int n = 7;
+        int answer = 0;
+
+        if(n % 2 == 1) {
+            for (int i = 0; i < n; i + 2) {
+                answer += i;
+            }
+        } else {
+            for (int i = 0; i <= n; i + 2) {
+                answer += (double)Math.pow(i, 2); 
+            }
+        }
     }
 }
